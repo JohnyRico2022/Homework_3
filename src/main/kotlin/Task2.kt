eb4ed1b3e6944cb9eb4b0645ea799d43da5e1291
Nikita Konstantinov <Nik-kita89@yandex.ru>
2023-05-09 10:58:38 +0300

    Change Task2

--- src/main/kotlin/Task2.kt
+++ src/main/kotlin/Task2.kt
@@ -1,80 +1,40 @@
 fun main() {
-    val paySystem = "VK Pay"
-    val pay = 1400
 
-    val moneyTransfer = fee(paySystem, pay)
+    val result = commission("Maestro", pay=80000)
 
-    println("Вы совершаете перевод на $pay руб.")
-    println("Выша комиссия по платежной системе $paySystem составляет: $moneyTransfer руб.")
-    println(limits(paySystem, pay))
-    println(limitsMonth(paySystem, pay))
-
-
+    if (result != -33.33)
+        println("Ваша комиссия за перевод: $result")
+    else
+        println("Вы превысили лимиты")
 }
 
-fun fee(paySystem: String, pay: Int): Double {
-    return when (paySystem) {
-        "Mastercard", "Maestro" -> {
-            if (pay < 75000) 0.0 else (pay / 100 * 0.6) + 20
-        }
+fun commission(paySystem: String = "VK Pay", payIntoMonth: Int = 0, pay: Int): Double {
 
-        "Visa", "Mir" -> {
-            val feeMinimum = 35
-            val fee = (pay / 100) * 0.75
-
-            return (if (fee < feeMinimum) feeMinimum else fee).toDouble()
-        }
-
-        else -> 0.0 //VK Pay
-
-    }
-
-}
-
-fun limits(paySystem: String, pay: Int): String {
-    val limitDayMax = 150_000
-    val limitDayVkMax = 15_000
-
-    when (paySystem) {
-        "VK Pay" -> {
-            if (pay <= limitDayVkMax)
-                return "Операция успешна, дневные лимиты не превышены"
-        }
-
-        else -> {
-            if (pay <= limitDayMax)
-                return "Операция успешна, дневные лимиты не превышены"
-        }
-    }
-    return "Превышены дневные лимиты"
-}
-
-
-fun limitsMonth(paySystem: String, pay: Int): String {
+    val monthPay = payIntoMonth + pay
     val limitMonthMax = 600_000
     val limitMonthVkMax = 40_000
     val limitDayMax = 150_000
     val limitDayVkMax = 15_000
 
-    when (paySystem) {
-        "VK Pay" -> {
-            val balance = limitMonthVkMax - pay
-            return if (pay <= limitMonthVkMax && pay <= limitDayVkMax)
-                "Ваш остаток по переводам в этом месяце составляет $balance руб. "
-            else ""
-
+    return when (paySystem) {
+        "Mastercard", "Maestro" -> {
+            if (pay <= limitDayMax && monthPay <= limitMonthMax) {
+                if (monthPay < 75000) 0.0 else (pay / 100 * 0.6) + 20
+            } else -33.33
         }
 
-        else -> {
+        "Visa", "Mir" -> {
+            if (pay <= limitDayMax && monthPay <= limitMonthMax) {
+                val feeMinimum = 35
+                val fee = (pay / 100) * 0.75
+                return (if (fee < feeMinimum) feeMinimum else fee).toDouble()
+            } else -33.33
+        }
 
-            val balance = limitMonthMax - pay
-            return if (pay <= limitMonthMax && pay <= limitDayMax)
-
-                "Ваш остаток по переводам в этом месяце составляет $balance руб. "
-            else ""
-
+        else -> {    //VK pay
+            if (pay <= limitDayVkMax && monthPay <= limitMonthVkMax) {
+                0.0
+            } else -33.33
         }
     }
-
-    return ""
 }
